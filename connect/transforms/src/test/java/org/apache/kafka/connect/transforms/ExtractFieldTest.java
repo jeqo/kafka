57bfcff98689b77693dbd96006d800d0435647f4
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-09-12 16:17:47 +0100

    feat: used FieldPath on ExtractField

--- connect/transforms/src/test/java/org/apache/kafka/connect/transforms/ExtractFieldTest.java
+++ connect/transforms/src/test/java/org/apache/kafka/connect/transforms/ExtractFieldTest.java
@@ -16,10 +16,12 @@
  */
 package org.apache.kafka.connect.transforms;
 
+import java.util.HashMap;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.transforms.util.FieldSyntaxVersion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -39,7 +41,7 @@ public class ExtractFieldTest {
     }
 
     @Test
-    public void schemaless() {
+    public void withSchemaless() {
         xform.configure(Collections.singletonMap("field", "magic"));
 
         final SinkRecord record = new SinkRecord("test", 0, null, Collections.singletonMap("magic", 42), null, null, 0);
@@ -50,7 +52,22 @@ public class ExtractFieldTest {
     }
 
     @Test
-    public void testNullSchemaless() {
+    public void withNestedSchemaless() {
+        Map<String, String> configs = new HashMap<>();
+        configs.put(Transformation.FIELD_SYNTAX_VERSION_CONFIG, FieldSyntaxVersion.V2.name());
+        configs.put("field", "magic.foo");
+        xform.configure(configs);
+
+        final Map<String, Object> key = Collections.singletonMap("magic", Collections.singletonMap("foo", 42));
+        final SinkRecord record = new SinkRecord("test", 0, null, key, null, null, 0);
+        final SinkRecord transformedRecord = xform.apply(record);
+
+        assertNull(transformedRecord.keySchema());
+        assertEquals(42, transformedRecord.key());
+    }
+
+    @Test
+    public void nullSchemaless() {
         xform.configure(Collections.singletonMap("field", "magic"));
 
         final Map<String, Object> key = null;
@@ -67,6 +84,23 @@ public class ExtractFieldTest {
 
         final Schema keySchema = SchemaBuilder.struct().field("magic", Schema.INT32_SCHEMA).build();
         final Struct key = new Struct(keySchema).put("magic", 42);
+        final SinkRecord record = new SinkRecord("test", 0, keySchema, key, null, null, 0);
+        final SinkRecord transformedRecord = xform.apply(record);
+
+        assertEquals(Schema.INT32_SCHEMA, transformedRecord.keySchema());
+        assertEquals(42, transformedRecord.key());
+    }
+
+    @Test
+    public void withNestedSchema() {
+        Map<String, String> configs = new HashMap<>();
+        configs.put(Transformation.FIELD_SYNTAX_VERSION_CONFIG, FieldSyntaxVersion.V2.name());
+        configs.put("field", "magic.foo");
+        xform.configure(configs);
+
+        final Schema fooSchema = SchemaBuilder.struct().field("foo", Schema.INT32_SCHEMA).build();
+        final Schema keySchema = SchemaBuilder.struct().field("magic", fooSchema).build();
+        final Struct key = new Struct(keySchema).put("magic", new Struct(fooSchema).put("foo", 42));
         final SinkRecord record = new SinkRecord("test", 0, keySchema, key, null, null, 0);
         final SinkRecord transformedRecord = xform.apply(record);
 
