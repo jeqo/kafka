ce10c96a2a98fccf9cbff0c2caf06f8771e347bd
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-09-13 11:37:46 +0100

    docs: add new field syntax config docs

--- connect/api/src/main/java/org/apache/kafka/connect/transforms/Transformation.java
+++ connect/api/src/main/java/org/apache/kafka/connect/transforms/Transformation.java
@@ -24,25 +24,31 @@ import java.io.Closeable;
 
 /**
  * Single message transformation for Kafka Connect record types.
- *
+ * <br/>
  * Connectors can be configured with transformations to make lightweight message-at-a-time modifications.
  */
 public interface Transformation<R extends ConnectRecord<R>> extends Configurable, Closeable {
 
-    String FIELD_SYNTAX_VERSION_CONFIG = "";
-    String FIELD_SYNTAX_VERSION_DOC = "";
+    String FIELD_SYNTAX_VERSION_CONFIG = "field.syntax.version";
+    String FIELD_SYNTAX_VERSION_DOC = "Defines the version of the syntax to access fields. "
+        + "If set to `V1`, then the field paths are limited to access the elements at the root level of the struct or map."
+        + "If set to `V2`, the syntax will support accessing nested elements. o access nested elements, "
+        + "dotted notation is used. If dots are already included in the field name, then backtick pairs "
+        + "can be used to wrap field names containing dots. "
+        + "e.g. to access elements from a struct/map named \"foo.bar\", "
+        + "the following format can be used to access its elements: \"`foo.bar`.baz\".";
 
     String FIELD_SYNTAX_VERSION_DEFAULT_VALUE = "V1";
 
     /**
      * Apply transformation to the {@code record} and return another record object (which may be {@code record} itself) or {@code null},
      * corresponding to a map or filter operation respectively.
-     *
+     * <br/>
      * A transformation must not mutate objects reachable from the given {@code record}
      * (including, but not limited to, {@link org.apache.kafka.connect.header.Headers Headers},
      * {@link org.apache.kafka.connect.data.Struct Structs}, {@code Lists}, and {@code Maps}).
      * If such objects need to be changed, a new ConnectRecord should be created and returned.
-     *
+     * <br/>
      * The implementation must be thread-safe.
      */
     R apply(R record);
